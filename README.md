46b4489b9cb82811d75bc2c8a73b4cf08e756c11
user <AbdulRaoufMKamal@gmail.com>
2022-12-11 22:55:06 +0200

    Added counter variable for image names in drive_rover.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rover Mars Project
+## Introduction
--- drive_rover.py
+++ drive_rover.py
@@ -77,6 +77,7 @@ class RoverState():
         self.near_sample = 0 # Will be set to telemetry value data["near_sample"]
         self.picking_up = 0 # Will be set to telemetry value data["picking_up"]
         self.send_pickup = False # Set to True to trigger rock pickup
+        self.counter = 1 
 # Initialize our rover 
 Rover = RoverState()
 
